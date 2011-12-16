3720faf55d5793f9a65c1c5be7a4bc64679c1cce
WarGoth <yavalek@gmail.com>
2011-12-15 22:35:34 -0800

    Adjusted ACRA fields. Limited report to just a few fields

--- src/org/sketcher/SketcherApplication.java
+++ src/org/sketcher/SketcherApplication.java
@@ -1,11 +1,15 @@
 package org.sketcher;
 
 import org.acra.ACRA;
+import org.acra.ReportField;
 import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 
-@ReportsCrashes(formKey = "dGFoVFJVVl9NQ0lNaFhKRW5sNmp6RHc6MQ")
+@ReportsCrashes(formKey = "dGFoVFJVVl9NQ0lNaFhKRW5sNmp6RHc6MQ", customReportContent = {
+		ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME,
+		ReportField.PACKAGE_NAME, ReportField.PHONE_MODEL,
+		ReportField.STACK_TRACE })
 public class SketcherApplication extends Application {
 	@Override
 	public void onCreate() {
